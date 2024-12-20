5d1dcf6bdfae5a1e942219b2412bf47e99e97011
TalalBnoob <79761207+TalalBnoob@users.noreply.github.com>
2024-12-20 16:11:43 +0300

    add create post btn

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,20 @@
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { PostFeed } from '../components'
 
 function HomePage() {
   return (
     <div className='w-screen h-screen'>
-      <nav className='bg-transparent 50 h-14 text-3xl flex justify-center items-center'>
+      <nav className='bg-transparent 50 h-14 text-3xl mr-auto flex justify-between items-center'>
+        {/* Filler div to make the layout as |space -- title -- btn| */}
+        <div className='w-10'></div>
         <h1>UST-C</h1>
+        <button className='text-sm px-2 py-1 rounded-sm bg-transparent'>
+          <FontAwesomeIcon
+            icon={faPenToSquare}
+            size='xl'
+          />
+        </button>
       </nav>
       <PostFeed />
     </div>
