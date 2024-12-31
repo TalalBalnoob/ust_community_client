a2897bb1094b7436dee1333c22e60109b754e3d4
TalalBnoob <79761207+TalalBnoob@users.noreply.github.com>
2024-12-31 13:47:57 +0300

    fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,48 @@
 import {
   createBrowserRouter,
   createRoutesFromElements,
+  Navigate,
   Route,
   RouterProvider,
 } from 'react-router-dom'
 import useAuth from './context/AuthProvider'
 import CreatePostPage from './pages/CreatePostPage'
 import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
 import ShowPostPage from './pages/ShowPostPage'
 import NotFoundPage from './pages/utils pages/NotFoundPage'
 import PrivateRoutes from './utils/PrivateRoutes'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route
-      element={<PrivateRoutes />}
-      errorElement={<NotFoundPage />}
-    >
-      <Route
-        element={<HomePage />}
-        path='/'
-      />
-      <Route
-        element={<CreatePostPage />}
-        path='/create'
-      />
-      <Route
-        element={<ShowPostPage />}
-        path='/posts/:postID'
-        errorElement={<NotFoundPage />}
-      />
-    </Route>,
-  ),
-)
-
 function App() {
   const { auth } = useAuth()
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route
+          element={<PrivateRoutes />}
+          errorElement={<NotFoundPage />}
+        >
+          <Route
+            element={<HomePage />}
+            path='/'
+          />
+          <Route
+            element={<CreatePostPage />}
+            path='/create'
+          />
+          <Route
+            element={<ShowPostPage />}
+            path='/posts/:postID'
+            errorElement={<NotFoundPage />}
+          />
+        </Route>
+        <Route
+          element={auth ? <Navigate to={'/'} /> : <LoginPage />}
+          path='/login'
+        />
+      </Route>,
+    ),
+  )
   return <RouterProvider router={router} />
 }
 
