c8cfb4991709e6a22746e8d68dfef208311f50f8
TalalBnoob <79761207+TalalBnoob@users.noreply.github.com>
2025-01-07 16:39:20 +0300

    profile work

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,18 +1,19 @@
 import { faHouse } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
 import Avatar from 'react-avatar'
 import {
   LoaderFunctionArgs,
   useLoaderData,
   useNavigate,
 } from 'react-router-dom'
-import { Button } from '../components'
+import { Button, PostFeed } from '../components'
 import { getAuth } from '../context/AuthProvider'
 import { staff, student, userProfile } from '../types'
 import { fetchUserProfile } from '../utils/api/fetchMethods'
 
 function UserProfilePage() {
-  const { username, user_type_id } = useLoaderData() as userProfile
+  const { user_type_id, followers, following } = useLoaderData() as userProfile
   type ProfileType<T extends number> = T extends 1
     ? userProfile<student>
     : T extends 2
@@ -23,14 +24,15 @@ function UserProfilePage() {
       ? (useLoaderData() as ProfileType<1>)
       : (useLoaderData() as ProfileType<2>)
   const navigate = useNavigate()
+  const [view, setView] = useState<'posts' | 'comments'>('posts')
 
   return (
-    <div className='w-screen h-screen'>
-      <nav className='bg-transparent h-14 text-3xl mr-auto flex justify-between items-center'>
+    <div className='h-screen w-screen'>
+      <nav className='mr-auto flex h-14 items-center justify-between bg-transparent text-3xl'>
         <div className='w-10'></div>
         <h1>UST-C</h1>
         <Button
-          className='text-sm px-2 mx-2 py-1 rounded-sm bg-transparent'
+          className='mx-2 rounded-sm bg-transparent px-2 py-1 text-sm'
           text={
             <FontAwesomeIcon
               icon={faHouse}
@@ -41,10 +43,10 @@ function UserProfilePage() {
           onClick={() => navigate('..')}
         />
       </nav>
-      <main className='w-[97%] m-x-auto'>
-        <div className='flex gap-x-3 text-right float-right'>
+      <main className='m-x-auto mt-6 w-full'>
+        <div className='flex w-full justify-end gap-x-3 text-right'>
           <div className='user_info flex flex-col'>
-            <h1 className='text-2xl flex items-baseline gap-x-2'>
+            <h1 className='flex items-baseline gap-x-2 text-2xl'>
               <p className='text-sm text-gray-200/40'>
                 {user_type_id === 1
                   ? `Level ${(profile as student).level}`
@@ -53,11 +55,16 @@ function UserProfilePage() {
               {profile.displayName}
             </h1>
             <div>
-              <h3 className='mt-2'>
+              <h3 className='mt-1'>
                 {user_type_id === 1
                   ? (profile as student).major
                   : (profile as staff).role}
               </h3>
+            </div>
+            <div className='mt-auto flex'>
+              {/* TODO: Add a btn that show the followers and the following */}
+              <div className='mr-auto'>المتابعين {followers}</div>
+              <div>المتابعين {following}</div>
             </div>
           </div>
           {true ? (
@@ -73,6 +80,38 @@ function UserProfilePage() {
               round={'100px'}
             />
           )}
+        </div>
+
+        <div className='my-6 flex flex-col items-end'>
+          {profile.bio ? (
+            <p>{profile.bio}</p>
+          ) : (
+            <p className='mx-auto text-gray-200/30'>لا يوجد وصف</p>
+          )}
+        </div>
+
+        <div className='mx-auto mt-4 flex w-full flex-col justify-end text-right'>
+          <div className='flex'>
+            <button
+              value={'comments'}
+              className='mx-0 h-8 w-full border border-gray-200/30'
+              onClick={() => setView('comments')}
+            >
+              التعليقات
+            </button>
+            <button
+              value={'posts'}
+              className='mx-0 h-8 w-full border border-gray-200/30'
+              onClick={() => setView('posts')}
+            >
+              المنشورات
+            </button>
+          </div>
+
+          <section>
+            {/* TODO: Make custom feed for the user profile  */}
+            {view === 'posts' ? <PostFeed /> : <h1>لا يوجد تعليقات</h1>}
+          </section>
         </div>
       </main>
     </div>
